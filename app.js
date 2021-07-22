9cc15fdb58c54238dd7cf551f5fdcd46d43a2029
hiromi0214 <81456689+hiromi0214@users.noreply.github.com>
2021-07-22 11:45:46 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const history = document.getElementById("history");
+const tt1 = document.getElementById("tt1");
+const tt2 = document.getElementById("tt2");
+const tt3 = document.getElementById("tt3");
+const tt4 = document.getElementById("tt4");
+const tt5 = document.getElementById("tt5");
+const tt6 = document.getElementById("tt6");
+const tt7 = document.getElementById("tt7");
+const tt8 = document.getElementById("tt8");
+const tt9 = document.getElementById("tt9");
+const answer1 = document.getElementById("answer1");
+
+tt7.addEventListener("click", open);
+        function open() {
+                history = document.write('<img src="https://thumb.ac-illust.com/ca/caf97bb080ca7b09930afee061c1115d_t.jpeg">');
+            }
+
+tt1.addEventListener("click", open2);
+tt2.addEventListener("click", open2);
+tt3.addEventListener("click", open2);
+tt4.addEventListener("click", open2);
+tt5.addEventListener("click", open2);
+tt6.addEventListener("click", open2);
+tt8.addEventListener("click", open2);
+tt9.addEventListener("click", open2);
+
+function open2(){
+    history = document.write('<img src="https://www.sozailab.jp/db_img/sozai/8270/33783bbdc29ad394c340b5441606a643.png">');
+}
